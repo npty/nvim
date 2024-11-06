28324a0ec8a7157d5c0c71d33a43ec22a351c415
npty <nptytn2@gmail.com>
2024-11-06 21:29:57 +0700

    feat: improve perf for nvim and add trouble

--- init.lua
+++ init.lua
@@ -92,6 +92,8 @@ vim.g.maplocalleader = ' '
 
 -- Set to true if you have a Nerd Font installed and selected in the terminal
 vim.g.have_nerd_font = true
+-- Limit the size of typescript server
+vim.env.TSS_MAX_MEMORY = '4096'
 
 -- [[ Setting options ]]
 -- See `:help vim.opt`
@@ -127,6 +129,15 @@ vim.opt.undofile = true
 -- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
+
+-- Ignore files for better memory usage
+vim.opt.wildignore:append {
+  'node_modules/*',
+  'dist/*',
+  'build/*',
+  '*.min.js',
+  '*.d.ts',
+}
 
 -- Keep signcolumn on by default
 vim.opt.signcolumn = 'yes'
@@ -245,6 +256,43 @@ require('lazy').setup({
   'sindrets/diffview.nvim',
   '0xmovses/move.vim',
   'ThePrimeagen/vim-be-good',
+  {
+    'folke/trouble.nvim',
+    opts = {}, -- for default options, refer to the configuration section for custom setup.
+    cmd = 'Trouble',
+    keys = {
+      {
+        '<leader>xx',
+        '<cmd>Trouble diagnostics toggle<cr>',
+        desc = 'Diagnostics (Trouble)',
+      },
+      {
+        '<leader>xX',
+        '<cmd>Trouble diagnostics toggle filter.buf=0<cr>',
+        desc = 'Buffer Diagnostics (Trouble)',
+      },
+      {
+        '<leader>cs',
+        '<cmd>Trouble symbols toggle focus=false<cr>',
+        desc = 'Symbols (Trouble)',
+      },
+      {
+        '<leader>cl',
+        '<cmd>Trouble lsp toggle focus=false win.position=right<cr>',
+        desc = 'LSP Definitions / references / ... (Trouble)',
+      },
+      {
+        '<leader>xL',
+        '<cmd>Trouble loclist toggle<cr>',
+        desc = 'Location List (Trouble)',
+      },
+      {
+        '<leader>xQ',
+        '<cmd>Trouble qflist toggle<cr>',
+        desc = 'Quickfix List (Trouble)',
+      },
+    },
+  },
   -- 'neoclide/coc.nvim',
   {
     'folke/twilight.nvim',
