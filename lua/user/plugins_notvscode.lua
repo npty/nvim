5efb703a9c23442ac099787205eccd18d0c491b3
npty <nptytn2@gmail.com>
2025-03-24 10:17:32 +0700

    chore: add picker

--- lua/user/plugins_notvscode.lua
+++ lua/user/plugins_notvscode.lua
@@ -23,11 +23,8 @@ return {
   {
     'folke/snacks.nvim',
     opts = {
-      notifier = {
-        -- your notifier configuration comes here
-        -- or leave it empty to use the default settings
-        -- refer to the configuration section below
-      },
+      notifier = {},
+      picker = {},
     },
   },
 
