050785df9d06eab41e24bcd0612c3028ccfc4210
npty <nptytn2@gmail.com>
2025-04-04 13:13:19 +0700

    refactor: remove unused code and comments in post_setup.lua

--- lua/custom/post_setup.lua
+++ lua/custom/post_setup.lua
@@ -87,64 +87,8 @@ function M.setup()
 
   require('tmux').setup()
 
-  -- The following two autocommands are used to highlight references of the
-  -- word under your cursor when your cursor rests there for a little while.
-  --    See `:help CursorHold` for information about when this is executed
-  --
-  --     -- When you move your cursor, the highlights will be cleared (the second autocommand).
-  --     local client = vim.lsp.get_client_by_id(event.data.client_id)
-  --     if client and client.supports_method(vim.lsp.protocol.Methods.textDocument_documentHighlight) then
-  --       local highlight_augroup = vim.api.nvim_create_augroup('kickstart-lsp-highlight', { clear = false })
-  --       vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
-  --         buffer = event.buf,
-  --         group = highlight_augroup,
-  --         callback = vim.lsp.buf.document_highlight,
-  --       })
-  --
-  --       vim.api.nvim_create_autocmd({ 'CursorMoved', 'CursorMovedI' }, {
-  --         buffer = event.buf,
-  --         group = highlight_augroup,
-  --         callback = vim.lsp.buf.clear_references,
-  --       })
-  --
-  --       vim.api.nvim_create_autocmd('LspDetach', {
-  --         group = vim.api.nvim_create_augroup('kickstart-lsp-detach', { clear = true }),
-  --         callback = function(event2)
-  --           vim.lsp.buf.clear_references()
-  --           vim.api.nvim_clear_autocmds { group = 'kickstart-lsp-highlight', buffer = event2.buf }
-  --         end,
-  --       })
-  --     end
-  --
-  --     -- The following code creates a keymap to toggle inlay hints in your
-  --     -- code, if the language server you are using supports them
-  --     --
-  --     -- This may be unwanted, since they displace some of your code
-  --     if client and client.supports_method(vim.lsp.protocol.Methods.textDocument_inlayHint) then
-  --       map('<leader>th', function()
-  --         vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled { bufnr = event.buf })
-  --       end, '[T]oggle Inlay [H]ints')
-  --     end
-  --   end,
-  -- })
-
-  -- LSP servers and clients are able to communicate to each other what features they support.
-  --  By default, Neovim doesn't support everything that is in the LSP specification.
-  --  When you add nvim-cmp, luasnip, etc. Neovim now has *more* capabilities.
-  --  So, we create new capabilities with nvim cmp, and then broadcast that to the servers.
   local original_capabilities = vim.lsp.protocol.make_client_capabilities()
   local capabilities = require('blink.cmp').get_lsp_capabilities(original_capabilities)
-  -- capabilities = vim.tbl_deep_extend('force', capabilities, require('cmp_nvim_lsp').default_capabilities())
-
-  -- Enable the following language servers
-  --  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
-  --
-  --  Add any additional override configuration in the following tables. Available keys are:
-  --  - cmd (table): Override the default command used to start the server
-  --  - filetypes (table): Override the default list of associated filetypes for the server
-  --  - capabilities (table): Override fields in capabilities. Can be used to disable certain LSP features.
-  --  - settings (table): Override the default settings passed when initializing the server.
-  --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
 
   require('aerial').setup {
     -- optionally use on_attach to set keymaps when aerial has attached to a buffer
@@ -189,13 +133,6 @@ function M.setup()
         server.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {})
         require('lspconfig')[server_name].setup(server)
       end,
-      -- You can add custom handlers for specific servers here if needed
-      -- For example:
-      -- lua_ls = function()
-      --   local server_opts = servers.lua_ls or {}
-      --   server_opts.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server_opts.capabilities or {})
-      --   require('lspconfig').lua_ls.setup(server_opts)
-      -- end,
     },
   }
 end
